ce5a4e5365483083afac7f7c51f2618055b5f0c9
iamangus <turnbull.angus@gmail.com>
2024-06-02 19:55:50 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ steamcmd +force_install_dir ~/vrising +login anonymous +app_update 1829350 +quit
 
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-xvfb-run -a wine ~/vrising/VRisingServer.exe -persistentDataPath ~/vrising/server-data
+xvfb-run -a wine ~/vrising/VRisingServer.exe -persistentDataPath ~/vrising/persistentdata

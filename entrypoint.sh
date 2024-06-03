4125f24cbdb025d96c4481069640e74b64bc02ee
iamangus <turnbull.angus@gmail.com>
2024-06-02 22:11:41 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ steamcmd +force_install_dir ~/vrising +login anonymous +app_update 1829350 +quit
 
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-xvfb-run -a wine ~/vrising/VRisingServer.exe -persistentDataPath ~/vrising/persistentdata
+xvfb-run -a wine ~/vrising/VRisingServer.exe -persistentDataPath ~/vrising/persistentdata -serverName "My V Rising Server" -saveName "world1" -logFile ".\logs\VRisingServer.log"
